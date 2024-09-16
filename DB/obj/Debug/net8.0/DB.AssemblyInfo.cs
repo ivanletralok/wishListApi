9d013f9360a8c10d8d7aef81408fbee6e937877f
ivanletralok <ivanletralok@gmail.com>
2024-09-15 23:04:09 -0500

    feat: change gitignore

--- DB/obj/Debug/net8.0/DB.AssemblyInfo.cs
+++ DB/obj/Debug/net8.0/DB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04cc37e8894d2ffe66e0301f67e2255c9f2c52a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TiendaOnlineAPI/obj/Debug/net8.0/OnlineShopAPI.AssemblyInfo.cs
+++ TiendaOnlineAPI/obj/Debug/net8.0/OnlineShopAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShopAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04cc37e8894d2ffe66e0301f67e2255c9f2c52a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShopAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShopAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
